49b6fc74774e75d4e154d591e9dfabbb8496ee6d
vishnu630 <vishnuteja779986@gmail.com>
2023-07-18 12:42:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express =require('express')
+
+const app =express()
+
+
+app.get('/',(req,res)=>{
+    res.send("<h2>hello world</h2>")
+})
+
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`server running in port ${port}`)
+})
